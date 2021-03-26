59da1d5132a58ebed4dc495378710bc0789a2fb4
Walter Smith <walter.c.smith01@gmail.com>
2021-03-26 14:09:19 -0400

    Feat:Added an conditional to the controller logic to print to the console if the user filled out the form completely or not

--- src/main/java/PizzaServlet.java
+++ src/main/java/PizzaServlet.java
@@ -17,8 +17,26 @@ public class PizzaServlet extends HttpServlet {
         String crustType = request.getParameter("crust_types");
         String sauceType = request.getParameter("sauce-type");
         String PieType = request.getParameter("size");
+        String[] toppings = request.getParameterValues("toppings");
+        String addressOfUser = request.getParameter("address");
 
 
-        System.out.println(crustType);
+        if (crustType != null && sauceType != null && PieType != null && toppings != null && addressOfUser != null){
+
+            System.out.println(crustType);
+            System.out.println(sauceType);
+            System.out.println(PieType);
+
+
+            for (int i = 0; i <= toppings.length - 1; i++){
+                System.out.println(toppings[i]);
+            }
+            System.out.println("Your address is: " + addressOfUser);
+        }   else {
+            System.out.println("User did not fill form completely");
+        }
+
+
+
     }
 }
