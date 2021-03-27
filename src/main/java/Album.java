af8ad660cd58a159039d4c4f43935058efbfb66d
Walter Smith <walter.c.smith01@gmail.com>
2021-03-26 21:15:17 -0400

    Feat: Created a bean, Album, for the albums table which includes the constructor, getters and setters

---
+++ src/main/java/Album.java
@@ -0,0 +1,62 @@
+import java.io.Serializable;
+import java.util.Date;
+
+
+//    JavaBean or model for album entity
+public class Album implements Serializable {
+
+    private String artist;
+    private String name;
+    private Date release_Date;
+    private double sales;
+    private String genre;
+
+    public Album(){ }
+
+//        getters and setters below
+
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getRelease_Date() {
+        return release_Date;
+    }
+
+    public void setRelease_Date(Date release_Date) {
+        this.release_Date = release_Date;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
+
+
+}
