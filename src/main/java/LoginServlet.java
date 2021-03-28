3b4721a819a35843f5297d6cb427c462d08c5af7
Walter Smith <walter.c.smith01@gmail.com>
2021-03-28 15:24:47 -0400

    Feat: Used a key to redirect unauthorized users that try to visit the /profile page. Allows the user to log out.

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession().getAttribute("user") != null) {
-            response.sendRedirect("/profile");
+            response.sendRedirect("profile");
             return;
         }
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
